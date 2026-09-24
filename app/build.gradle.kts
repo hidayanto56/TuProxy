f2c5a8ef2679bab2e51ed2d9d8c6dd1512a403e9
Taufik <real.hidayanto@gmail.com>
2026-09-24 11:39:03 +0700

    Bump versionCode 2 -> 3 (code 2 already used in Play track)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.tustudio.tuproxy"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
     }
 
